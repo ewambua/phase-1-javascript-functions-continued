8d77fd773940bd6de9a1f44c8b34b0a7c0911f0e
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-14 17:37:05 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+// Saturday Fun
+function saturdayFun(activity ='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+ }
+
+// Monday Work
+function mondayWork(workActivity = 'go to the office'){
+    return(`This Monday, I will ${workActivity}.`)
+  }
+
+// Wrap Adjective
+function wrapAdjective(highlight = '*'){
+    return function(adjective = 'a hard worker'){
+        return `You are ${highlight}${adjective}${highlight}!`
+    }
+}
